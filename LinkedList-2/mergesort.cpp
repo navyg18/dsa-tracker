70763847424096279dbba6ad39bafb2a7c5eec9f
navyg18 <112862379+navyg18@users.noreply.github.com>
2023-01-16 13:24:29 +0530

    Update mergesort.cpp

--- LinkedList-2/mergesort.cpp
+++ LinkedList-2/mergesort.cpp
@@ -1,13 +1,41 @@
-linkedlist mergeSort(Node* head, Node* tail ) {
-  if(head == tail){
-        linkedlist single;
-        single.addLast(head->data);
-        return single;
-      }
-
-      Node* mid = getMid(head, tail);
-      linkedlist first = mergeSort(head, mid);
-      linkedlist second = mergeSort(mid->next, tail);
-      linkedlist sorted = mergeTwoSortedLists(first, second);
-      return sorted;
-}
+ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+    ListNode*res=NULL;
+    if(head1==NULL){
+        return head2;
+    }
+    else if(head2==NULL){
+        return head1;
+    }
+    if(head1->val>=head2->val){
+        res=head2;
+        res->next=mergeTwoLists(head1,head2->next);
+    }
+    else{
+        res=head1;
+        res->next=mergeTwoLists(head1->next,head2);
+    }
+    
+    return res;
+    }
+  
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    if(head==NULL || head->next==NULL){
+        return head;
+      }   
+        ListNode *temp = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast !=  NULL && fast -> next != NULL){
+            temp = slow;
+            slow = slow->next;          
+            fast = fast->next->next;  
+            }   
+        temp->next=NULL;            
+      ListNode* first = sortList(head);
+      ListNode* second = sortList(slow);
+      return mergeTwoLists(first, second);
+      
+    }
+};
